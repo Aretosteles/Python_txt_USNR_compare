a1ddac9a7c6e0c0058f211f2ae436a0c81c75434
Aretosteles <76232104+Aretosteles@users.noreply.github.com>
2021-02-15 23:09:19 +0100

    Update project.py

--- project.py
+++ project.py
@@ -9,17 +9,20 @@ for g in b:
 	impb=g.split(' ')
 	if 'USNR'in impb[-1]:
 		if impb not in end_l: 
-			end_l.append(impb[-1])
+			yu=impb[-1].split('=')
+			end_l.append(yu[-1].rstrip('\n'))
 new_fl=open('c.txt','a')
 new_list=[]
 for h in range(l):
 	a_usnr=dfs['PartTXT'][h]
 	a_usnr=a_usnr.split(":")
-	new_list.append(a_usnr[-1])
+	new_list.append(a_usnr[-1].rstrip('\n'))
 
-c=0
-for el in new_list:
-	c+=1
-	if el in new_list:
-		new_fl.write(str(c)+' '+el+'\n') 
-
+c=1
+for x in new_list:
+	if x in end_l:
+		new_fl.write(str(c)+' '+'USNR'+x+' '+'USN'+x)
+		new_fl.write('\n')
+	else:
+		new_fl.write(str(c)+' '+'USNR'+x+' '+'NA')
+		new_fl.write('\n')
